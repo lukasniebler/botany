d424be2e185b82868c846b2a454011cdc3e7c2b1
Lukas Niebler <nieblerlukas@gmail.com>
2022-07-12 19:58:18 +0200

    require once

--- includes/Main.php
+++ includes/Main.php
@@ -9,8 +9,8 @@ class Main
     public function __construct($pluginFile)
     {
         $this->pluginFile = $pluginFile;
-        // require_once __DIR__ . '../vendor/cmb2/init.php';
-        Helper::debug(__DIR__ . '../vendor/cmb2/cmb2/init.php');
+        require_once plugin_dir_path($pluginFile) . 'vendor/cmb2/cmb2/init.php';
+        //Helper::debug(plugin_dir_path($pluginFile) . 'vendor/cmb2/cmb2/init.php');
         new Cpt($pluginFile);
         new Helper();
     }
